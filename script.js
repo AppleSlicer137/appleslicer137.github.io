f7b4831f52ca928475662b4b47541631917e6212
Tim Delrue <tim.delrue@infosupport.com>
2024-11-02 17:29:39 +0100

    Testing secrets

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('myButton').addEventListener('click', function() {
     console.log('Button clicked! Tim\'s world!');
+    console.log('Environment Variable TEST_SECRET:', process.env.TEST_SECRET);
 });
-
 console.log('Tim\'s world!');
